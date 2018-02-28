e41295fd9f29044bd1cb3ecdb8ee02f70fc26c1d
yaminmhd <mdyamin.drzee@gmail.com>
2018-02-28 13:49:20 +0800

    Implemented fizzbuzz with test cases

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,4 @@
-/* FizzBuzz. 
+/* FizzBuzz.
 
 For a given natural number greater than zero return:
 - 'fizz' if the number is dividable by 3
@@ -8,7 +8,15 @@ For a given natural number greater than zero return:
 */
 
 function fizzbuzz(number) {
-
+  if(number % 15 === 0){
+    return 'fizzbuzz';
+  }else if(number % 3 === 0){
+    return 'fizz';
+  }else if(number % 5 === 0){
+    return 'buzz';
+  }else{
+    return number;
+  }
 }
 
 module.exports = fizzbuzz
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,9 +1,21 @@
 const fizzbuzz = require("../src/fizzbuzz");
 
-test("1 should return number 1", function() {
+test.skip("1 should return number 1", function() {
   expect(fizzbuzz(1)).toEqual(1);
 });
 
-test.skip("3 should return fizz", function() {
+test("3 should return fizz", function() {
   expect(fizzbuzz(3)).toEqual("fizz");
+});
+
+test("5 should return buzz", function() {
+  expect(fizzbuzz(5)).toEqual("buzz");
+});
+
+test("15 should return fizzbuzz", function() {
+  expect(fizzbuzz(15)).toEqual("fizzbuzz");
+});
+
+test("Numbers that don't meet requirement should return input number", function() {
+  expect(fizzbuzz(2)).toEqual(2);
 });
